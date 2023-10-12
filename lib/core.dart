b5c8ee0a783667e8f7b3b28c64fd06f9be6f3d67
6362002612Ramya <131331477+6362002612Ramya@users.noreply.github.com>
2023-10-12 23:44:57 +0530

    Add files via upload

---
+++ lib/core.dart
@@ -0,0 +1,18 @@
+/*
+We believe, the class name must be unique.
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:chatapp/authenticate/authenticate.dart';
+export 'package:chatapp/authenticate/createaccount.dart';
+export 'package:chatapp/authenticate/loginscreen.dart';
+export 'package:chatapp/authenticate/methods.dart';
+export 'package:chatapp/groupchat/addmembers.dart';
+export 'package:chatapp/groupchat/addmembers1.dart';
+export 'package:chatapp/groupchat/creategroup.dart';
+export 'package:chatapp/groupchat/groupchatroom.dart';
+export 'package:chatapp/groupchat/groupchatscreen.dart';
+export 'package:chatapp/groupchat/groupinfo.dart';
+export 'package:chatapp/screens/chatroom.dart';
+export 'package:chatapp/screens/homescreen.dart';
+export 'package:chatapp/widget.dart';
